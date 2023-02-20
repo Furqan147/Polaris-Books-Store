53188e301f777350427c0401b8a21c67dbfc2dbc
Furqan <furqan_zfr@outlook.com>
2023-02-21 00:18:24 +0500

    First controller added.

---
+++ PolarisBooks.StoreManagement.API/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PolarisBooks.StoreManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        #region Properties
+
+
+        #endregion
+
+        #region Constructor
+
+
+        #endregion
+
+
+        #region Public Actions
+
+
+        #endregion
+
+    }
+}
